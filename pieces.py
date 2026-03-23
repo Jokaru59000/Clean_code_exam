fcac56287d38841e5d3cd7f48836d68252152c57
Arnaud.m <45336018+Jokaru59000@users.noreply.github.com>
2026-03-23 09:24:42 +0100

    Ajout de la classe Piece

---
+++ pieces.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 09:12:58 2026
+
+@author: arnau
+
+Gestion des différentes pièces du bâtiment
+"""
+
+class Piece:
+    def __init__ (self,nom) : 
+        self.nom = nom
