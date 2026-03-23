6b1fd2311c84eff3994dd926d988cfb9ef7a7f00
Arnaud.m <45336018+Jokaru59000@users.noreply.github.com>
2026-03-23 10:17:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,30 @@ Consigne :
 """
 
 from capteurs import CameraA, TemperatureSensorA, ThermalSensorB, ThermalSensorB_Adapter
+from notifications import Email_Notification, Log_Notification, Discord_Notification
+from pieces import Piece
 
+
+def main():
+    desk = Piece("Bureau")
+    cantine = Piece("Cafétéria")
+    
+    desk.add_notif(Email_Notification('arnaud@gmail.com'))
+    desk.add_notif(Log_Notification())
+    desk.add_notif(Discord_Notification('nono_du_59'))
+    
+    cantine.add_notif(Log_Notification())
+    cantine.add_notif(Email_Notification('arnaud@gmail.com'))
+    
+    
+    desk_camera = CameraA("Camera_bureau")
+    desk.add_sensor(desk_camera)
+    
+    cantine_thermalsensor = ThermalSensorB_Adapter("Capteur_thermique_cafet")
+    cantine.add_sensor(cantine_thermalsensor)
+    
+    desk_camera.callback("Mouvement detecté")
+    cantine_thermalsensor.sensor_b.trigger_heat_signature(lambda date: None)
+    
+if __name__ == "__main__":
+    main()
