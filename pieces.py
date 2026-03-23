fa7e02d5263c718849a9b3c54f37be883fbb3e27
Arnaud.m <45336018+Jokaru59000@users.noreply.github.com>
2026-03-23 10:17:33 +0100

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
 Created on Mon Mar 23 09:12:58 2026
 
@@ -10,3 +10,24 @@ Gestion des différentes pièces du bâtiment
 class Piece:
     def __init__ (self,nom) : 
         self.nom = nom
+        self.sensor = []
+        self.notifications = []
+
+# On ajoute le système de notifications à la pièce  
+
+    def add_notif(self, notifications):
+        self.notifications.append(notifications)
+        
+# On ajoute un ou plsrs capteurs par pièce
+    
+    def add_sensor(self, sensor):
+        self.sensor.append(sensor)
+        sensor.on_detect(self.notify_everyone)
+        
+# On ajoute un systeme pour envoyer un message à tout le monde
+
+    def notify_everyone(self, message):
+        full_message = f"Alerte dans {self.nom} : {message}"
+        for n in self.notifications:
+            n.send(full_message)
+
